855369d2c694524f3faff8a70ce98cd568efe07c
Markus Kurbel <m4rku@pm.me>
2023-03-10 20:16:22 +0100

    Finalize site style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import Greeting from './components/Greeting.vue'
 import AWS from 'aws-sdk';
 import { ref, onMounted } from 'vue';
 
@@ -23,12 +22,13 @@ const buttons = [
               href: "https://github.com",
               color: "#ffffff"
             },
-            {
+/*            {
               name: "Twitter",
               icon: "fa-brands fa-twitter",
               href: "https://twitter.com",
               color: "#1DA1F2"
             },
+            */
             {
               name: "LinkedIn",
               icon: "fa-brands fa-linkedin",
@@ -36,17 +36,17 @@ const buttons = [
               color: "#0077B5"
             },
             {
+              name: "Xing",
+              icon: "fa-brands fa-xing",
+              href: "https://xing.com",
+              color: "#026466"
+            },
+            {
               name: "Mail",
               icon: "fa-solid fa-envelope",
               href: "https://github.com",
               color: "#DB4437"
             },
-            {
-              name: "Xing",
-              icon: "fa-brands fa-xing",
-              href: "https://xing.com",
-              color: "#026466"
-            }
       ];
 
 AWS.config.update({
@@ -57,20 +57,30 @@ AWS.config.update({
     region: import.meta.env.VITE_REGION,
     s3BucketEndpoint: true,
 });
+
 const ep = new AWS.Endpoint(import.meta.env.VITE_S3_BUCKET_ENDPOINT);
-
 const s3 = new AWS.S3({ signatureVersion: 'v4', endpoint: ep });
-const bucketName = 'website-assets'
-
-const objectKey = 'background.jpg';
 
 const backgroundImage = ref('');
+const logoImage = ref('');
+const favicon = ref('');
+
+async function getS3ImageUrl(objectKey: string): Promise<string> {
+  const bucketName = 'website-assets'
+  const imageData = await s3.getObject({ Bucket: `${bucketName}`, Key: objectKey }).promise();
+  return URL.createObjectURL(new Blob([imageData.Body as string], { type: imageData.ContentType }));
+}
 
 onMounted(async () => {
   try {
-    const data = await s3.getObject({ Bucket: `${bucketName}`, Key: objectKey }).promise();
-    const url = URL.createObjectURL(new Blob([data.Body as string], { type: data.ContentType }));
-    backgroundImage.value = url;
+    const bgObjectKey = 'background.jpg';
+    backgroundImage.value = await getS3ImageUrl(bgObjectKey);
+
+    const logoObjectKey = 'me.jpg';
+    logoImage.value = await getS3ImageUrl(logoObjectKey);
+
+    const faviconObjectKey = 'favicon.ico';
+    favicon.value = await getS3ImageUrl(faviconObjectKey);
   } catch (error) {
     console.error(error);
   }
@@ -78,48 +88,81 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div :style="{ backgroundImage: `url(${backgroundImage})` }">
-  <v-container class="bg-surface-variant">
-    <v-row align="center" justify="center" class="ma-4 pa-4" no-gutters>
-        <img alt="Vue logo" src="./assets/logo.svg" width="125" height="125" />
-    </v-row>
-    <v-row align="center" justify="center" class="ma-2 pa-2" no-gutters>
-      <v-col cols="4">
-        <Greeting msg="Hello I'm Foo Bar" />
-      </v-col>
-    </v-row>
-    <v-row 
-      align="center"
-      justify="center"
-      no-gutters
-      v-for="button in buttons">
-      <v-col cols="4" class="pa-1 ma-1">
-        <v-btn
-          class="v-btn mx-1 px-6"
-          block
-          :color="button.color"
-          :href="button.href">
-          <v-icon :icon="button.icon" class="ma-1" />
-            {{ button.name }}
-        </v-btn>
-      </v-col>
-    </v-row>
-  </v-container> 
-  </div>
+<!--  <div class="background" :style="{ backgroundImage: `url(${backgroundImage})` }"> -->
+    <v-container class="bg-surface-variant">
+      <v-row align="center" justify="center" class="ma-2 pa-2" no-gutters>
+          <v-avatar size="250" rounded>
+            <img alt="Profile pic" :src="logoImage" width="250" height="250" />
+          </v-avatar>
+      </v-row>
+      <v-row align="center" justify="center" class="ma-2 pa-2" no-gutters>
+        <v-col cols="3">
+          <div class="greetings">
+            <h1 class="green">Hi, I'm Markus</h1>
+            <h3>
+              I like Software Engineering
+            </h3>
+          </div>
+
+        </v-col>
+      </v-row>
+      <v-row 
+        align="center"
+        justify="center"
+        no-gutters
+        v-for="button in buttons">
+        <v-col cols="3" class="pa-1 ma-1">
+          <v-btn
+            class="v-btn mx-1 px-6"
+            block
+            :color="button.color"
+            :href="button.href">
+            <v-icon :icon="button.icon" class="ma-1" />
+              {{ button.name }}
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container> 
+    <link rel="shortcut icon" :href="favicon" type="image/x-icon" />
+<!-- </div> -->
 </template>
 
 <style scoped>
 
 @media (min-width: 1024px) {
-  .logo {
-    margin-left: 50%;
-  }
-
 .v-btn:hover,
 .v-btn:focus {
   box-shadow: 0 0.5em 0.5em -0.4em var(--hover);
   transform: translateY(-0.25em);
   color: rgba(0, 0, 0, 0.3);
 }
+}
+
+.background{
+  background-size: 100% 100%;
+  opacity: 1;
+  height: 100vh;
+}
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: center;
+  }
 }
 </style>
