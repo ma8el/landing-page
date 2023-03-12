8a182fd0561e6d297917b97ec4d86b6c895600eb
Markus Kurbel <m4rku@pm.me>
2023-03-12 14:19:39 +0100

    Readd api url variable

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ const buttons = ref<Button[]>([]);
 onMounted(async () => {
   try {
 //    const apiUrl = import.meta.env.API_URL;
-    const apiUrl = "https://db.m4k.site";
+    const apiUrl = import.meta.env.VITE_API_URL;
     const jwtToken = import.meta.env.VITE_JWT_TOKEN;
     const response = await axios.get<Button[]>(`${apiUrl}/socials`, {
       headers: {
